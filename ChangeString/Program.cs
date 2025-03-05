4a1ff6668b814def5fe720c6f9fcb76902c70225
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-03-05 19:02:42 +0000

    Added user Input

---
+++ ChangeString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ChangeString
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string cOut = Console.ReadLine();
+
+            char chosenChar = char.Parse(cOut);
+
+            char c;
+
+
+
+        }
+    }
+}
