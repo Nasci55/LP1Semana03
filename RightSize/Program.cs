eb95d866b9ab466157aff1b6adc6c1ec6cf7209a
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-03-05 18:38:04 +0000

    Check if args are more than 3 chars

---
+++ RightSize/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RightSize
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            foreach (string w in args)
+            {
+                if (w.Length > 3)
+                {
+                    Console.WriteLine(w);
+                }
+            }
+        }
+    }
+}
