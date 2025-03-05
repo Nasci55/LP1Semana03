2b1378c406513ffe7ea680af604189f98da3890f
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-03-05 23:09:05 +0000

    Finished the program

--- ArrayMul/Program.cs
+++ ArrayMul/Program.cs
@@ -10,18 +10,37 @@ namespace ArrayMul
             float[,] matrixA = new float[2, 2];
 
 
-            matrixA[0, 0] = float.Parse(args[0]);
-            matrixA[0, 1] = float.Parse(args[1]);
-            matrixA[1, 0] = float.Parse(args[2]);
-            matrixA[1, 1] = float.Parse(args[3]);
+            matrixA[0, 0] = float.Parse(args[0], CultureInfo.InvariantCulture);
+            matrixA[0, 1] = float.Parse(args[1], CultureInfo.InvariantCulture);
+            matrixA[1, 0] = float.Parse(args[2], CultureInfo.InvariantCulture);
+            matrixA[1, 1] = float.Parse(args[3], CultureInfo.InvariantCulture);
 
             float[,] matrixB = new float[2, 1];
 
-            matrixB[0, 0] = float.Parse(args[4]);
-            matrixB[0, 1] = float.Parse(args[5]);
+            matrixB[0, 0] = float.Parse(args[4], CultureInfo.InvariantCulture);
+            matrixB[1, 0] = float.Parse(args[5], CultureInfo.InvariantCulture);
 
             float[,] result = new float[2, 1];
 
+            for (int l = 0; l < 2; l++)
+            {
+                for (int j = 0; j < 1; j++)
+                {
+                    result[l, j] = 0;
+                    for (int c = 0; c < 2; c++)
+                    {
+                        result[l, j] += matrixA[l, c] * matrixB[c, j];
+                    }
+                }
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 1; j++)
+                {
+                    Console.WriteLine($"| {result[i, j],7:F2} |");
+                }
+            }
 
 
         }
