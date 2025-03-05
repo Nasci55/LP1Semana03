27c093d00ec99b94a0b176d65825da21a9555c47
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-03-05 20:40:03 +0000

    Added first matrix

---
+++ ArrayMul/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ArrayMul
+{
+    public class Program
+    {
+        private static void Main(float[] args)
+        {
+            float[,] matrixA = new float[2, 2];
+
+            matrixA[0, 0] = args[0];
+            matrixA[0, 1] = args[1];
+            matrixA[1, 0] = args[2];
+            matrixA[1, 1] = args[3];
+
+
+        }
+    }
+}
