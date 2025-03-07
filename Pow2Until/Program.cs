8ca0306cddcfe706310e37d4ebc45992524f9389
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-03-07 07:05:51 +0000

    Created pow2Until and create a new method

---
+++ Pow2Until/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters;
+
+namespace Pow2Until
+{
+    public class Program
+    {
+        private static void PowersOf2Until5()
+        {
+            for (int i = 1; i <= (1 << 5); i = i << 1)
+            {
+                System.Console.WriteLine(i);
+            }
+        }
+
+
+
+        private static void Main(string[] args)
+        {
+
+        }
+    }
+}
