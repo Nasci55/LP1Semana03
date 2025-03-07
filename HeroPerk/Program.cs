80fc017169912ebc4256dec83a50ca58d7ab2712
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-03-07 05:13:04 +0000

    Added switch cases for each option

--- HeroPerk/Program.cs
+++ HeroPerk/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using HeroPerks;
 
 namespace HeroPerk
 {
@@ -6,7 +7,51 @@ namespace HeroPerk
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            string stealth = "s";
+            char stealthChar = char.Parse(stealth);
+
+            string warpShift = "d";
+            char warpShiftChar = char.Parse(warpShift);
+
+            string doubleJump = "w";
+            char doubleJumpChar = char.Parse(doubleJump);
+
+            string autoHeal = "a";
+            char autoHealChar = char.Parse(autoHeal);
+
+            byte perk = 0b0000;
+
+            foreach (string c in args)
+            {
+                foreach (char s in c)
+                {
+                    switch (s)
+                    {
+                        case 'w':
+                            perk |= (byte)Perks.DoubleJump;
+                            break;
+                        case 'd':
+                            perk |= (byte)Perks.WarpShift;
+                            break;
+                        case 'a':
+                            perk |= (byte)Perks.AutoHeal;
+                            break;
+                        case 's':
+                            perk |= (byte)Perks.Stealth;
+                            break;
+                        default:
+                            System.Console.WriteLine("!Unknown perk");
+                            break;
+
+
+                    }
+
+                }
+
+
+            }
+
+            System.Console.WriteLine(perk);
         }
     }
 }
